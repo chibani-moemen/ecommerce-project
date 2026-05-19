17284774252b6abb829130a4680f41c345d9fc1d
Houssem <haloui.houssem.123@gmail.com>
2026-05-19 10:41:19 +0100

    Add image upload for meubles

--- src/Controller/MeubleAdminController.php
+++ src/Controller/MeubleAdminController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/meuble/admin')]
 final class MeubleAdminController extends AbstractController
@@ -23,17 +24,29 @@ final class MeubleAdminController extends AbstractController
     }
 
     #[Route('/new', name: 'app_meuble_admin_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $meuble = new Meuble();
         $form = $this->createForm(MeubleType::class, $meuble);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('kernel.project_dir') . '/public/uploads/meubles',
+                    $newFilename
+                );
+                $meuble->setImage($newFilename);
+            }
+
             $entityManager->persist($meuble);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_meuble_admin_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_meuble_admin_index');
         }
 
         return $this->render('meuble_admin/new.html.twig', [
@@ -51,15 +64,27 @@ final class MeubleAdminController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_meuble_admin_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Meuble $meuble, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Meuble $meuble, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(MeubleType::class, $meuble);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('kernel.project_dir') . '/public/uploads/meubles',
+                    $newFilename
+                );
+                $meuble->setImage($newFilename);
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_meuble_admin_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_meuble_admin_index');
         }
 
         return $this->render('meuble_admin/edit.html.twig', [
@@ -71,11 +96,11 @@ final class MeubleAdminController extends AbstractController
     #[Route('/{id}', name: 'app_meuble_admin_delete', methods: ['POST'])]
     public function delete(Request $request, Meuble $meuble, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$meuble->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $meuble->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($meuble);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_meuble_admin_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_meuble_admin_index');
     }
 }
--- src/Form/MeubleType.php
+++ src/Form/MeubleType.php
@@ -6,24 +6,40 @@ use App\Entity\Categorie;
 use App\Entity\Meuble;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class MeubleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
-            ->add('prix')
-            ->add('stock')
-            ->add('image')
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('prix', NumberType::class)
+            ->add('stock', IntegerType::class)
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image (JPG, PNG)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/webp'],
+                        'mimeTypesMessage' => 'Veuillez uploader une image valide',
+                    ])
+                ],
+            ])
             ->add('categorie', EntityType::class, [
                 'class' => Categorie::class,
-                'choice_label' => 'id',
-            ])
-        ;
+                'choice_label' => 'nom',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
